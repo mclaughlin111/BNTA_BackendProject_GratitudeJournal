38ca07dbb51e44bfe4d73030a39fc03f3a934935
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-11-13 16:48:44 +0000

    created Java file with spring io

---
+++ GratitudeJournal/src/main/java/com/group6/GratitudeJournal/GratitudeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.group6.GratitudeJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeJournalApplication.class, args);
+	}
+
+}
---
+++ GratitudeJournal/src/test/java/com/group6/GratitudeJournal/GratitudeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.GratitudeJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratitudeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
